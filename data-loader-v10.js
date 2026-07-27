6acf6262fd561a6f0151cf86c9f4b109c088262b
4k29 <2hjpkx4564@privaterelay.appleid.com>
2026-07-27 20:18:24 +0900

    Refresh paginated database app cache

--- data-loader-v10.js
+++ data-loader-v10.js
@@ -135,7 +135,7 @@
 
   async function loadApp() {
     await loadScript('./ios-sync-fix-v12.js?v=14');
-    await loadScript('./app-v9.js?v=17');
+    await loadScript('./app-v9.js?v=18');
   }
 
   async function start() {
