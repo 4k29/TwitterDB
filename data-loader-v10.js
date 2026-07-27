ba45a73202944c355ca3e99f90f3fa2ba6d99d5f
4k29 <2hjpkx4564@privaterelay.appleid.com>
2026-07-27 16:43:10 +0900

    Refresh cumulative count app cache

--- data-loader-v10.js
+++ data-loader-v10.js
@@ -135,7 +135,7 @@
 
   async function loadApp() {
     await loadScript('./ios-sync-fix-v12.js?v=14');
-    await loadScript('./app-v9.js?v=16');
+    await loadScript('./app-v9.js?v=17');
   }
 
   async function start() {
